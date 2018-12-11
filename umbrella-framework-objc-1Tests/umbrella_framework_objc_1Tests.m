0e052f7d1a77a690aedf9168fbf85c8237b44b97
Quang V. Ha <quang.ha@rakuten.com>
2018-12-11 10:22:37 +0800

    Initial Commit

---
+++ umbrella-framework-objc-1Tests/umbrella_framework_objc_1Tests.m
@@ -0,0 +1,37 @@
+//
+//  umbrella_framework_objc_1Tests.m
+//  umbrella-framework-objc-1Tests
+//
+//  Created by Ha, Quang on 2018/12/11.
+//  Copyright © 2018 quangDecember@Github. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface umbrella_framework_objc_1Tests : XCTestCase
+
+@end
+
+@implementation umbrella_framework_objc_1Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
